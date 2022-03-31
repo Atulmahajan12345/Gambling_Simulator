54a2989db7c645df1c49637ac727c3918fc02028
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-03-31 11:30:13 +0530

    Welcome

---
+++ src/com/bridgelabz/javaproject/Welcome.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.javaproject;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		// TODO Auto-generated method stub
+
+	}
+
+}
